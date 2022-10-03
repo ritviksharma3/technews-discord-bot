f1555923ca8cee913f1448191d886fdd11cdf24e
ritvik <ritviksharma3@gmail.com>
2022-10-03 21:30:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Tech news Discord Bot
+
+### More about this website
+
+Develop a discord bot which sends new Tech news after every 24 Hours
+
+### Tech stack
+
+- NodeJs
+
+### Rules to contribute
+
+> - Fork and clone this repository
+> - Make changes and if you have added any tech stack add it in README
+> - Create an issue and send PR
---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const intents = new Discord.Intents(32509);
+const client = new Discord.Client({ intents }, {disableEveryone: false});
+
+client.on("ready", () => {
+    console.log('The bot is online')    
+})
+
+client.on("messageCreate", message => {
+    if(message.content === "ping"){
+        message.channel.send('Hello Wolrld!')
+    }
+});
+
+client.login(config.token);
