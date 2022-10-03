b83500671e27c75a22d8d47357efab7e7eb02a66
ritvik <ritviksharma3@gmail.com>
2022-10-03 21:40:21 +0530

    Initial commit

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 
 const intents = new Discord.Intents(32509);
-const client = new Discord.Client({ intents }, {disableEveryone: false});
+const client = new Discord.Client({ intents }, { disableEveryone: false });
 
 client.on("ready", () => {
-    console.log('The bot is online')    
-})
+  console.log("The bot is online");
+});
 
-client.on("messageCreate", message => {
-    if(message.content === "ping"){
-        message.channel.send('Hello Wolrld!')
-    }
+client.on("messageCreate", (message) => {
+  if (message.content === "ping") {
+    message.channel.send("Hacktoberfest");
+  }
 });
 
 client.login(config.token);
